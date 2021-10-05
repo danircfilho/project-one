6d50e536936bbd6008248c64f55bb0218df9ef59
danircfilho <danircfilho@hotmail.com>
2021-10-05 17:14:47 -0300

    server modifications

---
+++ server.js
@@ -0,0 +1,38 @@
+/*Import*/
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.json({
+        'success' : true
+    });
+});
+
+app.get('/testdb', (req,res) => {
+    //example acess db
+    const data = [
+        {
+            id: 1,
+            name: 'Project 1',
+            createdAt: '2021-01-01' 
+        },
+        {
+            id: 2,
+            name: 'Project 2',
+            createdAt: '2021-01-01' 
+        },
+        {
+            id: 3,
+            name: 'Project 3',
+            createdAt: '2021-01-01' 
+        }
+    ];
+        res.json({
+            success : true,
+            data: data
+        });
+}); 
+
+const PORT = 3080;
+app.listen(PORT);
