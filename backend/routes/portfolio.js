9ac11a6abc46e2dc3e6beb355609c8a49cc79362
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2021-10-11 19:42:44 -0300

    crud - patch

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,99 +1,93 @@
-const router = require('express').Router();
-const Portfolio = require('../models/Portfolio'); 
+const router = require("express").Router();
+const Portfolio = require("../models/Portfolio");
 
 /* CRUD */
 
 /* Create */
-router.post('/', async (req, res) => {
-    const portfolio = new Portfolio({
-        title: req.body.title,
-        description: req.body.description
-    })
-  
-    try {
-        const savedPortfolio = await portfolio.save()
-        res.json ({
-            success: true,
-            data: savedPortfolio
-        })
+router.post("/", async (req, res) => {
+  const portfolio = new Portfolio({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-    }catch(err) {
-        res,json ({
-            success: false,
-            message: err
-        })
-    }
+  try {
+    const savedPortfolio = await portfolio.save();
+    res.json({
+      success: true,
+      data: savedPortfolio,
+    });
+  } catch (err) {
+    res,
+      json({
+        success: false,
+        message: err,
+      });
+  }
 });
 
 /* Read */
-router.get('/', async (req, res) => {
-    try {
-        const portfolio = await Portfolio.find({
-            
-        })
+router.get("/", async (req, res) => {
+  try {
+    const portfolio = await Portfolio.find({});
 
-        res.json ({
-            success : true,
-            data: portfolio
-        })
+    res.json({
+      success: true,
+      data: portfolio,
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err,
+    });
+  }
+});
 
-    }catch(err) {
-        res.json ({
-            success: false,
-            message: err
-        })
-    }    
-}); 
+router.get("/:slug", async (req, res) => {
+  try {
+    const portfolio = await Portfolio.findOne({
+      slug: req.params.slug,
+    });
 
-router.get('/:slug', async (req, res) => {
-    try {
-        const portfolio = await Portfolio.findOne({
-            slug: req.params.slug
-        })
-
-        res.json ({
-            success : true,
-            data: portfolio
-        })
-
-    }catch(err) {
-        res.json ({
-            success: false,
-            message: err
-        })
-    }    
-}); 
+    res.json({
+      success: true,
+      data: portfolio,
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err,
+    });
+  }
+});
 
 /* Update */
-router.patch('/:slug', async (req, res) => {    
-    try {
-        const updatePortfolio = await Portfolio.updateOne(
-            {
-                slug: req.params.slug
-            },
-            {
-                title: req.body.title,
-                require: req.body.require
-        })
+router.patch("/:slug", async (req, res) => {
+  try {
+    const updatedPortfolio = await Portfolio.updateOne(
+      {
+        slug: req.params.slug,
+      },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+        },
+      }
+    );
 
-        res.json ({
-            success: true,
-            updated: updatedPortfolio.nModified
-        })
-
-    }catch(err) {
-        res.json ({
-            success: false,
-            message: err
-        })
-    }
-}); 
+    res.json({
+      success: true,
+      updated: updatedPortfolio.nModified,
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err,
+    });
+  }
+});
 
 /* Delete */
 
-
-
-
 /*Exports*/
-module.exports = router
-
+module.exports = router;
