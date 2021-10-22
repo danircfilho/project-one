a4b024a4d67dd4af34d6e365606db3339c656bfc
danircfilho <danircfilho@hotmail.com>
2021-10-22 16:16:19 -0300

    card creation

--- frontend/src/pages/Portfolio.js
+++ frontend/src/pages/Portfolio.js
@@ -1,11 +1,85 @@
 import React from "react";
+import styled from "styled-components";
 
 const Portfolio = () => {
     return (
-        <div>
-            Portfolio
-        </div>
+        <PortfolioList>
+            <CardList>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+                <StyleCard>
+                    <img src="" />
+                    <div>
+                        <h3>Title</h3>
+                        <h4>Date</h4>
+                        <p>Short description</p>
+                    </div>
+                </StyleCard>
+            </CardList>
+        </PortfolioList>
     )
 }
+
+const PortfolioList = styled.div`
+    min-height: 90vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+`;
+
+const CardList = styled.div`
+    background-color: red;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
+
+const StyleCard = styled.div`
+    background-color: green;
+    min-height: 30vh;
+    border-color: aliceblue;
+    text-align: center;
+    border-radius: 1rem;
+    cursor: pointer;
+    overflow: hidden;
+`;
 
 export default Portfolio
