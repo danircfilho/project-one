fb14fbf77b5a4c09a422e77f374c8cf409692993
danircfilho <danircfilho@hotmail.com>
2021-11-09 17:53:45 -0300

    forms

--- frontend/src/components/FormContact.js
+++ frontend/src/components/FormContact.js
@@ -1,51 +1,73 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import moment from "moment";
+import { save } from "../services/sheet"
 
 const FormContact = () => {
     //preencher formulario
     const [ name, setName ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ message, setMessage ] = useState('');
+    const [ success, setSuccess ] = useState(false);
+    const [ err, setErr ] = useState(false);
+
+    const resetForm = () => {
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
 
     //capturar os dados
-    const contactMe = (e) => {
+    const contactMe = async (e) => {
+        e.preventDefault()
+
         const data = {
             Name: name,
             Email: email,
             Message: message,
             Date: moment().format('DD/MM/YYYY'),
         }
+
+        const result = await save(data);
+
+        if (result) {
+            setSuccess(result)
+            resetForm()
+        }else {
+            setErr(!result)
+        }   
     }
 
     return (
         <Form onSubmit={contactMe}>
-                <Input>
-                    <label>Name:</label>  
-                    < input 
-                        type = "text" 
-                        value = {name}
-                        onChange = { (e)=>{setName(e.target.value)} }
-                    />  
-                </Input>
-                <Input>
-                    <label>E-mail:</label>  
-                    <input
-                        type = "email" 
-                        value = {email}
-                        onChange = { (e)=>{setEmail(e.target.value)} } 
-                        
-                    />
-                </Input>
-                <Input>
-                    <label>Message:</label>  
-                    < textarea 
-                        rows="5" 
-                        value = {message}
-                        onChange = { (e)=>{setMessage(e.target.value)} } 
-                    /> 
-                </Input>              
-                <button type="submit">Submit</button>                     
+            <Input>
+                <label>Name:</label>  
+                < input 
+                    type = "text" 
+                    value = {name}
+                    onChange = { (e)=>{setName(e.target.value)} }
+                />  
+            </Input>
+            <Input>
+                <label>E-mail:</label>  
+                <input
+                    type = "email" 
+                    value = {email}
+                    onChange = { (e)=>{setEmail(e.target.value)} } 
+                    
+                />
+            </Input>
+            <Input>
+                <label>Message:</label>  
+                < textarea 
+                    rows="5" 
+                    value = {message}
+                    onChange = { (e)=>{setMessage(e.target.value)} } 
+                /> 
+            </Input>              
+            <button type="submit">Submit</button> 
+            {success && <h4>Mensagem enviada com sucesso!</h4>}                    
+            {err && <h4>Ocorreu um erro! Por favor, envie a mensagem novamente!</h4>}                    
         </Form>        
     )
 }
@@ -60,6 +82,9 @@ const Form = styled.form`
             color: grey;
             } 
         }
+    h4 {
+        margin-top: 1rem;
+    }
 `;
 
 const Input = styled.div`
--- frontend/src/services/sheet.js
+++ frontend/src/services/sheet.js
@@ -1,10 +1,26 @@
 /* Configurar o axios para não necessitar configurar toda vez que for efetuar uma chamada */
 // Não será usado React
+import axios from "axios";    
 
-import axios from "axios";
+export const save = (data) => {
+    const config = {
+        method: 'post',
+        url: 'https://sheet.best/api/sheets/c3826b0b-4c49-4951-96c4-d0141d37e869',
+        headers: {
+            "Content-Type": "application/JSON",
+        },
+        data
+    };
 
-const api = axios.create({
-    baseURL: 'https://sheet.best/api/sheets/c3826b0b-4c49-4951-96c4-d0141d37e869'
-})
+    axios (config)
+    .then(res => {
+        console.log("Dados da Planilha", res)
+        return true
+    })
+    .catch(err => {
+        console.log(err)
+        return false
+    })
+}
 
-export default api
+
